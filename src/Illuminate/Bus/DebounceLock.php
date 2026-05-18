f525f5903402c06ceb9a2525280e4d3f48d326b5
Jack Bayliss <jjbayliss@icloud.com>
2026-05-18 15:30:48 +0100

    Revert "drop old bitz"
    This reverts commit c9ee3b29b4d8d3cab54b5e6b80b0716ef907f26c.

--- src/Illuminate/Bus/DebounceLock.php
+++ src/Illuminate/Bus/DebounceLock.php
@@ -109,6 +109,17 @@ class DebounceLock
     }
 
     /**
+     * Determine if a debounce token exists for the given job.
+     *
+     * @param  mixed  $job
+     * @return bool
+     */
+    public function lockExists($job)
+    {
+        return ! is_null($this->getCurrentOwner($job));
+    }
+
+    /**
      * Remove the debounce token for the given job.
      *
      * @param  mixed  $job
